897cf6f2c7031eaad1d6a61989a17f4ecb95a42f
Kate van Lopik <katevanlopik@Kates-MacBook-Pro.local>
2016-12-20 17:42:11 +0000

    some more models and js to go with them
    pallets
    booking in desk

---
+++ public_html/js/deskwithPallet.js
@@ -0,0 +1,28 @@
+function createDeskChairManBoxStood(x, y, z, rotation, id, callback) {
+    var loader = new THREE.ObjectLoader();
+    loader.load('images/DeskChairManBoxStood.json', function(obj) {
+      obj.rotateY(rotation);
+        obj.scale.set(8,8,8)
+
+        obj.position.x = x;
+        obj.position.y = y;
+        obj.position.z = z;
+
+
+        obj.name = id;
+
+        var _thisPerson = {
+            id: id,
+            object: obj,
+            cube: {
+                cube_x: x,
+                cube_y: y + 6,
+                cube_z: z
+            },
+            work_in_progress: []
+
+        }
+
+        callback(_thisPerson);
+    });
+}
---
+++ public_html/js/palletGreyBoxes.js
@@ -0,0 +1,28 @@
+function createPalletGrey(x, y, z, rotation, id, callback) {
+    var loader = new THREE.ObjectLoader();
+    loader.load('images/palletDarkBoxes.json', function(obj) {
+      obj.rotateY(rotation);
+        obj.scale.set(8,8,8)
+
+        obj.position.x = x;
+        obj.position.y = y;
+        obj.position.z = z;
+
+
+        obj.name = id;
+
+        var _thisPerson = {
+            id: id,
+            object: obj,
+            cube: {
+                cube_x: x,
+                cube_y: y + 6,
+                cube_z: z
+            },
+            work_in_progress: []
+
+        }
+
+        callback(_thisPerson);
+    });
+}
