1b6d200df86720c155a7d5c48aa61bebf81d85bd
Kate van Lopik <katevanlopik@Kates-MacBook-Pro.local>
2017-02-20 13:53:24 +0000

    data erasure room finished
    DE desk with phones on charge, and printer for DE certs

---
+++ public_html/js/dataErase.js
@@ -0,0 +1,28 @@
+function createdataErase(x, y, z, rotation, id, callback) {
+    var loader = new THREE.ObjectLoader();
+    loader.load('images/dataErase.json', function(obj) {
+      obj.rotateY(rotation);
+        obj.scale.set(9,9,9)
+
+        obj.position.x = x;
+        obj.position.y = y;
+        obj.position.z = z;
+
+
+        obj.name = id;
+
+        var _thisPerson = {
+            id: id,
+            object: obj,
+            cube: {
+                cube_x: x,
+                cube_y: y + 6,
+                cube_z: z
+            },
+            work_in_progress: []
+
+        }
+
+        callback(_thisPerson);
+    });
+}
