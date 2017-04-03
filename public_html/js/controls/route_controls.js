f5a694c425a31bc78eab11ebbd1bd2a3a53c2386
rich175 <rich.g.sharpe@gmail.com>
2017-04-03 16:52:49 +0100

    Moves to locations

--- public_html/js/controls/route_controls.js
+++ public_html/js/controls/route_controls.js
@@ -101,7 +101,7 @@ var prevTime = performance.now();
 
 function moveRoute(_route, x, y, z) {
 
-  var velocity = new THREE.Vector3();
+    var velocity = new THREE.Vector3();
     var timeWindowS = 0;
     var timeWindowE = 0;
 
@@ -144,12 +144,12 @@ function moveRoute(_route, x, y, z) {
 
         timeWindowS = timeWindowE;
         timeWindowE = timeWindowE + parseInt(_thisRoute.route.path[i].movement.t);
-        console.log('Elapsed Time:' + timeElapsed);
-        console.log('Window:' + timeWindowS + ' - ' + timeWindowE);
+        //console.log('Elapsed Time:' + timeElapsed);
+        //console.log('Window:' + timeWindowS + ' - ' + timeWindowE);
 
         if (timeElapsed < timeWindowE && timeElapsed > timeWindowS) {
             //WE ARE HERE IN THE ROUTE
-            console.log('In Window');
+            console.log('In Window: ' + i);
             foundWindow = true;
 
             var time = performance.now();
@@ -161,25 +161,40 @@ function moveRoute(_route, x, y, z) {
             var moveUp = false;
             var moveDown = false;
 
-            console.log('Moving');
 
-            var error = 5;
 
+            var error = 0.5;
+
+            var _smooth = 0.2;
+            var _fast = 1;
+
+            var _distanceLimit = 100;
+
+            var _xMove = _smooth;
+            var _yMove = _smooth;
+            var _zMove = _smooth;
+
+            // IF desired location is bigger / smaller than current
             if (_thisRoute.route.path[i].movement.x > (currentX + error)) {
                 moveForward = true;
-                console.log('x+             from:' + currentX + ' to: ' + _thisRoute.route.path[i].movement.x);
+                if ((_thisRoute.route.path[i].movement.x - currentX) > _distanceLimit) _xMove = _fast;
+                console.log('x+             from:' + currentX + ' to: ' + _thisRoute.route.path[i].movement.x + ' Speed:' + _xMove);
             }
             if (_thisRoute.route.path[i].movement.x < (currentX - error)) {
                 moveBackward = true;
-                console.log('x-             from:' + currentX + ' to: ' + _thisRoute.route.path[i].movement.x);
+                if ((currentX - _thisRoute.route.path[i].movement.x) > _distanceLimit) _xMove = _fast;
+                console.log('x-             from:' + currentX + ' to: ' + _thisRoute.route.path[i].movement.x + ' Speed:' + _xMove);
             }
             if (_thisRoute.route.path[i].movement.z > (currentZ + error)) {
                 moveRight = true;
-                console.log('z+             from:' + currentZ + ' to: ' + _thisRoute.route.path[i].movement.z);
+                if ((_thisRoute.route.path[i].movement.z - currentZ) > _distanceLimit) _zMove = _fast;
+                console.log('z+             from:' + currentZ + ' to: ' + _thisRoute.route.path[i].movement.z + ' Speed:' + _zMove);
             }
             if (_thisRoute.route.path[i].movement.z < (currentZ - error)) {
                 moveLeft = true;
-                console.log('z-             from:' + currentZ + ' to: ' + _thisRoute.route.path[i].movement.z);
+                if ((currentZ - _thisRoute.route.path[i].movement.z) > _distanceLimit) _zMove = _fast;
+                console.log('z-             from:' + currentZ + ' to: ' + _thisRoute.route.path[i].movement.z + ' Speed:' + _zMove);
+
             }
             if (_thisRoute.route.path[i].movement.y > (currentY + error)) {
                 moveUp = true;
@@ -190,14 +205,24 @@ function moveRoute(_route, x, y, z) {
                 console.log('y-             from:' + currentY + ' to: ' + _thisRoute.route.path[i].movement.y);
             }
 
-            var _smooth = 1;
 
-            if (moveBackward) results.X = currentX - _smooth;
-            if (moveForward) results.X = currentX + _smooth;
-            if (moveLeft) results.Z = currentZ - _smooth;
-            if (moveRight)results.Z = currentZ + _smooth;
-            if (moveUp) results.Y = currentY + _smooth;
-            if (moveDown) results.Y = currentY - _smooth;
+
+            if (moveBackward) results.X = currentX - _xMove;
+            if (moveForward) results.X = currentX + _xMove;
+            if (moveLeft) {
+                results.Z = currentZ - _zMove;
+                console.log(currentZ);
+                console.log(results.Z);
+            }
+            if (moveRight) results.Z = currentZ + _zMove;
+            if (moveUp) results.Y = currentY + _yMove;
+            if (moveDown) results.Y = currentY - _yMove;
+
+            if (!moveBackward && !moveForward && !moveLeft && !moveRight && !moveUp && !moveDown) {
+                //console.log('Not Moving');
+            } else {
+                console.log('Moving');
+            }
             break;
         }
 
@@ -210,10 +235,9 @@ function moveRoute(_route, x, y, z) {
         //results.Z = currentZ + (velocity.z * 10);
         //results.translateX = velocity.x * delta;
         //results.translateY = velocity.y * delta;
-      //  results.translateZ = velocity.z * delta;
-    }
-    else {
-      results.complete = true;
+        //  results.translateZ = velocity.z * delta;
+    } else {
+        results.complete = true;
     }
     prevTime = time;
     return results;
